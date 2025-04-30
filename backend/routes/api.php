fd8a89e5f1fc041b5cdca4faa082306709d26bb9
Jonathan Vandal <jonathan.vandal@leptech.net>
2025-04-30 19:02:29 +0200

    prettier

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Route::middleware('auth:sanctum')->get('/api/user', function (Request $request) {
+//     return $request->user();
+// });
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
