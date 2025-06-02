e4dfc0320fbfdf2e5e0d721c5e0bf02b3d0c2ee8
johngtrs <vandal.jonathan@gmail.com>
2025-06-02 12:13:33 +0200

    change user seeder name

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,9 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // UserSeeder::class,
             BandSeeder::class,
             VenueSeeder::class,
+            // UserSeeder::class,
         ]);
     }
 }
